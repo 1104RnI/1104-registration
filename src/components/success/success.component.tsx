b8ccd972931130127745e1fbe5a6cd519623ef2b
ED <granrr.z@gmail.com>
2024-04-29 19:32:09 +0900

    State management conflict between Success component and useForwardProgress hook resolved

--- src/components/success/success.component.tsx
+++ src/components/success/success.component.tsx
@@ -1,26 +1,26 @@
-import { useEffect } from 'react'
+// import { useEffect } from 'react'
 
 import Lottie from 'lottie-react'
 import successAnim from '../../assets/lottie/successAnim.json'
 
-import { useProgressStore } from '../../store/progressStore'
+// import { useProgressStore } from '../../store/progressStore'
 
 import { SuccessContainer } from './success.styles'
 
 export default function Success() {
-	const updateRequestState = useProgressStore(
-		(state) => state.updateRequestStatus,
-	)
+	// const updateRequestState = useProgressStore(
+	// 	(state) => state.updateRequestStatus,
+	// )
 
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			updateRequestState('idle')
-		}, 1500)
+	// useEffect(() => {
+	// 	const timer = setTimeout(() => {
+	// 		updateRequestState('idle')
+	// 	}, 1500)
 
-		return () => {
-			clearTimeout(timer)
-		}
-	}, [updateRequestState])
+	// 	return () => {
+	// 		clearTimeout(timer)
+	// 	}
+	// }, [updateRequestState])
 
 	return (
 		<SuccessContainer>
--- src/hooks/useForwardProgress.tsx
+++ src/hooks/useForwardProgress.tsx
@@ -4,22 +4,26 @@ import { useProgressStore } from '../store/progressStore'
 const useForwardProgress = (delay: number = 1500) => {
 	const requestStatus = useProgressStore((state) => state.requestStatus)
 	const forwardProgress = useProgressStore((state) => state.forwardProgress)
+	const updateRequestState = useProgressStore(
+		(state) => state.updateRequestStatus,
+	)
 
 	useEffect(() => {
-		let timeoutId: NodeJS.Timeout | null = null
+		let timeoutId: number | null = null
 
 		if (requestStatus === 'success') {
-			timeoutId = setTimeout(() => {
+			timeoutId = window.setTimeout(() => {
 				forwardProgress()
+				updateRequestState('idle')
 			}, delay)
 		}
 
 		return () => {
-			if (timeoutId) {
-				clearTimeout(timeoutId)
+			if (timeoutId !== null) {
+				window.clearTimeout(timeoutId)
 			}
 		}
-	}, [requestStatus, forwardProgress, delay])
+	}, [requestStatus, forwardProgress, delay, updateRequestState])
 }
 
 export default useForwardProgress
