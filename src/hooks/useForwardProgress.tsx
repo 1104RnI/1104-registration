5c26666e8dfeaf19fda2591a889b26c0e4e04a7c
ED <granrr.z@gmail.com>
2024-04-29 18:46:41 +0900

    Live test 3

--- src/hooks/useForwardProgress.tsx
+++ src/hooks/useForwardProgress.tsx
@@ -4,13 +4,13 @@ import { useProgressStore } from '../store/progressStore'
 const useForwardProgress = (delay: number = 1500) => {
 	const requestStatus = useProgressStore((state) => state.requestStatus)
 	const forwardProgress = useProgressStore((state) => state.forwardProgress)
-	const timeoutIdRef = useRef<NodeJS.Timeout | null>(null)
+	const timeoutIdRef = useRef<number | null>(null)
 
 	useEffect(() => {
 		console.log(1)
 		if (requestStatus === 'success') {
 			console.log(2)
-			timeoutIdRef.current = setTimeout(() => {
+			timeoutIdRef.current = window.setTimeout(() => {
 				console.log(3)
 				forwardProgress()
 			}, delay)
@@ -18,7 +18,7 @@ const useForwardProgress = (delay: number = 1500) => {
 
 		return () => {
 			if (timeoutIdRef.current) {
-				clearTimeout(timeoutIdRef.current)
+				window.clearTimeout(timeoutIdRef.current)
 			}
 		}
 	}, [requestStatus, forwardProgress, delay])
